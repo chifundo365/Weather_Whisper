02d40a489a0d03ad8d99d72dd050a2c894e2577b
chifundo biziweck <chifundo@biziweckgmail.com>
2024-07-23 13:47:55 +0000

    Backend cod for the messages engine

--- sms_service.py
+++ sms_service.py
@@ -8,7 +8,6 @@ from sib_api_v3_sdk.rest import ApiException
 from pprint import pprint
 
 configuration = sib_api_v3_sdk.Configuration()
-configuration.api_key['api-key'] = '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'
 
 api_instance = sib_api_v3_sdk.TransactionalSMSApi(sib_api_v3_sdk.ApiClient(configuration))
 send_transac_sms = sib_api_v3_sdk.SendTransacSms(sender="Weatherwh", recipient="265994396986", content="Weather details for today", type="transactional")

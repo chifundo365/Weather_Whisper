f18080d288fd409c618ce902993c0872c5b09fa1
chifundo biziweck <chifundo@biziweckgmail.com>
2024-07-24 10:23:37 +0000

    moved and renamed file for Flask

--- app.py
+++ app.py
@@ -36,7 +36,10 @@ def home_page():
         return render_template("subscribe.html", errors=True)
     
 
-
+@app.route("/subsribe",  methods=["GET"], strict_slashes=False)
+def subscribe():
+    """ subscribe page """
+    return render_template("subscribe.hmtl")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

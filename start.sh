73572a183a07644de304e9768b010a23586edd37
chifundo biziweck <chifundobiziweck@gmail.com>
2025-09-26 15:40:57 +0200

    Update API keys in start.sh for Flask web app launch

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # starts a flask web app with necesssary data
-python3 app.py  
+APIIP_API_KEY=617ddb8f-f252-40a6-a723-1ab3e433f8be WEATHERBIT_API_KEY=bb732992d09e4a0bbb7f9e2c62996ad7 python app.py  

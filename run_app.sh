d510e58e4169e6800695fd0e9fbb2cc0e135241a
chifundo biziweck <chifundobiziweck@gmail.com>
2025-09-26 15:23:21 +0200

    Refactor HTML templates for improved navigation and styling; update API keys in run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # starts a flask web app with necesssary data
-APIIP_API_KEY=f3cdbfa6-3875-460e-979d-ca3548cc6d45 WEATHERBIT_API_KEY=5ac1518d91b84558b55cc3bbe2890437 python app.py  
+APIIP_API_KEY=617ddb8f-f252-40a6-a723-1ab3e433f8be WEATHERBIT_API_KEY=bb732992d09e4a0bbb7f9e2c62996ad7 python app.py  
